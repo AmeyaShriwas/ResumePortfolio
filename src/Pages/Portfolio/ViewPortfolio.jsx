f22261d915fb1f972e618aa50d5c21f94aff9b95
AmeyaShriwas133 <shriwas.ameya@webseeder.in>
2025-02-23 12:39:34 +0530

    portfolio

--- src/Pages/Portfolio/ViewPortfolio.jsx
+++ src/Pages/Portfolio/ViewPortfolio.jsx
@@ -31,7 +31,7 @@ const ViewPortfolio = () => {
   }
 
   return (
-    <div className="container-fluid p-0" style={{ background: "#F5F5F5", minHeight: "100vh" }}>
+    <div className="container-fluid p-0" style={{ background: "white", minHeight: "100vh" }}>
       {/* Header */}
       <header className="d-flex justify-content-between align-items-center bg-dark text-light p-3">
         <h4 className="m-0">{data.name}'s Portfolio</h4>
